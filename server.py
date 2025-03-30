8272f3bcd579f95cf88e06436a1be3c67ee7fb4c
Thomas Gallauner <classified>
2025-03-30 13:11:51 -0400

    Task 8.

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+"""Flask app for emotion decoder app."""
 from flask import Flask, request, render_template
 from EmotionDetection import emotion_detector
 
@@ -5,11 +6,24 @@ app = Flask(__name__)
 
 @app.route('/')
 def render_index_page():
+    """
+    Renders the index.html page.
+
+    Returns:
+        render_template function for index.html
+    """
     return render_template('index.html')
 
 
 @app.route('/emotionDetector', methods=['GET'])
 def detect_emotion():
+    """
+    Calls the emotion detection API, identifes the dominant
+    emotion and returns the result as a dictionary.
+
+    Returns:
+        dict: Dictionary with the results and identifeid dominant emotion.
+    """
     input_text = request.args.get('textToAnalyze', '')
 
     result = emotion_detector(input_text)
